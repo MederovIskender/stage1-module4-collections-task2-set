2842194994b2c8c55d87b05b2591a7c9f82f69f1
Iskender <mederov.iskendr@gmail.com>
2023-07-17 12:52:54 +0600

    first commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -2,8 +2,26 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> sortedSet = new HashSet<>();
+        for (int number: sourceList){
+            if (number % 2 == 0){
+                int current = number;
+                while (current % 2 == 0){
+                    sortedSet.add(current);
+                    current /=2;
+                }
+                sortedSet.add(current);
+            } else {
+                sortedSet.add(number);
+                sortedSet.add(2 * number);
+            }
+        }
+        return sortedSet;
     }
+
 }
+
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,19 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+
+        Set<String> result = new HashSet<>();
+
+        Set<String> interSection = new HashSet<>(firstSet);
+        interSection.retainAll(secondSet);
+        interSection.removeAll(thirdSet);
+        result.addAll(interSection);
+
+        Set<String> exclusiveToThird = new HashSet<>(thirdSet);
+        exclusiveToThird.removeAll(firstSet);
+        exclusiveToThird.removeAll(secondSet);
+        result.addAll(exclusiveToThird);
+
+        return result;
     }
 }
